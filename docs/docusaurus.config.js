f6d20af9343687df1707538f4833c392b6c6d8c6
rbasharacogility <rbashara@cogility.com>
2023-03-28 18:07:35 -0700

    different action to deploy docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://rbasharacogility.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/action-testing/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rbasharacogility', // Usually your GitHub org/user name.
-  projectName: 'rbasharacogility.github.io', // Usually your repo name.
+  projectName: 'action-testing', // Usually your repo name.
   deploymentBranch: 'master',
   trailingSlash: false,
 
